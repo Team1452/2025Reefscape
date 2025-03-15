5b344a93c313ff16f15740ef2238755c0676e7cf
ESchless <eitanschlessinger@gmail.com>
2025-03-15 00:28:56 -0700

    Format and constants

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,6 +34,7 @@ public class Robot extends LoggedRobot {
   private RobotContainer robotContainer;
 
   public Robot() {
+
     // Record metadata
     Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
     Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
